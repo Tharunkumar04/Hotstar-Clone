4dbeca12d7f91ba2a61cb2c9f383ddf381da7ba0
Kollu Tharun Kumar <114304982+Tharunkumar04@users.noreply.github.com>
2023-08-02 14:02:19 +0530

    adding index files

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Hotstar-Clone
+# Disney+ Hotstar Clone
+
+### Tech Stacks
+* HTML
+* CSS
+* JavaScript
+
+### Platforms used
+* Visual Studio Code
+
+### Features
+* carousel slider created by using JavaScript.
+* card with background video.
+* whenever we hover on card it display movie name and its description.
+* previous and next buttons slidind effect created by using JavaScript.
+
+
+
+#### More Featurs Coming Soon...🙂🙂🙂
+
+#### Thanks...!
+kollutharun kumar
---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name:'loki',
+        des:'The mercurial villain Loki resumes his role as the God of Mischief in a new series that takes place after the events of “Avengers: Endgame.”',
+        image:'images/slider 1.PNG'
+    },
+    {
+        name:'KIRAN',
+        des:'DANCER AND STUDENT OF SVPCET',
+        image:'images/KIRAN.jpg'
+    },
+    {
+        name:'avinash',
+        des:'DANCER AND STUDENT OF SVPCET',
+        image:'images/avi.jpg'
+    },
+    {
+        name:'BHARGAV',
+        des:'Selected in Mavel character AND STUDENT OF SVPCET ',
+        image:'images/BBR.jpg'
+    },
+    {
+        name:'KARTHIK',
+        des:'Data Scientist  AND STUDENT OF SVPCET ',
+        image:'images/KARTHIK.jpg'
+    }
+]
